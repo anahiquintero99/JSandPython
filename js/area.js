c2d385ab097b54101826d53b317140deb6e6bbbc
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-08 18:23:52 -0500

    New exercises about area to triangle

---
+++ js/area.js
@@ -0,0 +1,6 @@
+function areaTriangle(base = 24, height = 13) {
+  area = (base * height) / 2;
+  console.log(area);
+}
+
+areaTriangle();
---
+++ python/area.py
@@ -0,0 +1,5 @@
+base = int(input('Base: '))
+height = int(input('Altura: '))
+
+area = (base * height) / 2
+print(area)
