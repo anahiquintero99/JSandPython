6238bbc6193ace88bb1efa6bd2fb3b937836c820
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-08 18:08:09 -0500

    New exercises about spending of sweets

---
+++ js/sweet.js
@@ -0,0 +1,12 @@
+function consumptionSweet(tsweets = 100, lsweets = 13) {
+  consumption = tsweets - lsweets;
+  console.log(consumption);
+}
+
+function spendingSweet() {
+  spending = consumption * 13;
+  console.log(spending);
+}
+
+consumptionSweet();
+spendingSweet();
---
+++ python/sweet.py
@@ -0,0 +1,8 @@
+tdulces = int(input('Total de dulces: '))
+sdulces = int(input('Dulces sobrantes: '))
+
+consumo = tdulces - sdulces
+gasto = consumo * 13
+
+print(f'Consumo: {consumo}')
+print(f'Gasto: {gasto}')
