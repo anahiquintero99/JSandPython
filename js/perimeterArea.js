6cc05e25e8b531a112b4ce25c96e722526e920de
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-07 21:25:50 -0500

    New exercises about perimeter and surface to rectangle

---
+++ js/perimeterArea.js
@@ -0,0 +1,12 @@
+function perimeterRectangle(base = 24, height = 13) {
+  perimeter = (base + height) * 2;
+  console.log(perimeter);
+}
+
+function surfaceRectangle(base = 24, height = 13) {
+  surface = base * height;
+  console.log(surface);
+}
+
+perimeterRectangle();
+surfaceRectangle();
---
+++ python/perimeterArea.py
@@ -0,0 +1,7 @@
+base = int(input('Ingrese base: '))
+height = int(input('Ingrese altura: '))
+
+perimeter = (base + height) * 2
+surface = base * height
+
+print(perimeter, surface)
