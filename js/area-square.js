b2cc0f4635ca914d670d4cd370ac0ff752771782
Anahi Quintero <anitaquintero99@gmail.com>
2022-01-03 21:23:56 -0600

    new exercise, area square

---
+++ js/area-square.js
@@ -0,0 +1,12 @@
+function areaSquare(side = 4) {
+  const area = side * side;
+  console.log(area);
+}
+
+function perimeterSquare(side = 4) {
+  const perimeter = side * 4;
+  console.log(perimeter);
+}
+
+areaSquare();
+perimeterSquare();
