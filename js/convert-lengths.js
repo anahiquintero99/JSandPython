aab4611399eba9087cd8eb01d376ec7a05fcc5b1
Anahi Quintero <anitaquintero99@gmail.com>
2022-01-04 20:39:57 -0600

    centimeters to inch

---
+++ js/convert-lengths.js
@@ -0,0 +1,7 @@
+function convertLengths(length = 13) {
+  const inch = 2.54;
+  const centimeters = length / inch;
+  console.log(length + " centimetros = " + centimeters + " pulgadas");
+}
+
+convertLengths();
