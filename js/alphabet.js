57aefd1a3146b3aa72ebaf6b346761a09cd8ede1
Anahi Quintero <anitaquintero99@gmail.com>
2022-01-11 22:04:32 -0600

    new exercise, alphabet in js

---
+++ js/alphabet.js
@@ -0,0 +1,14 @@
+function Alphabet(pangram) {
+  let alphabet = String.fromCharCode(...Array(123).keys())
+    .slice(97)
+    .split(",");
+  pangramLowerCase = pangram.toLowerCase;
+  for (const letter of alphabet) {
+    const result = alphabet.filter((letter) => letter === pangramLowerCase);
+  }
+
+  console.log(alphabet);
+}
+
+pangram = "Cwm fjord bank glyphs vext quiz";
+Alphabet(pangram);
