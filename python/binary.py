126421e51da5b9d44234f701c088d5c19785b4f5
Anahi Quintero <anitaquintero99@gmail.com>
2022-01-13 22:15:55 -0600

    new exercise, binary

--- python/binary.py
+++ python/binary.py
@@ -1,12 +1,13 @@
 
 def binary_array_to_number():
-    arr = [0, 0, 0, 1]
+    arr = [1, 1, 1, 1, 0, 0, 0, 1, 0]
     arr.reverse()
+    result = 0
     for indice in range(0, len(arr)):
         value = arr[indice] * pow(2, indice)
-        suma = suma + value
-
-    print(suma)
+        result = result + value
+    print(arr)
+    print(result)
 
 
 binary_array_to_number()
