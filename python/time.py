8913d5c59a7a0c5f02606ec255d3b86ad92dca70
Anahi Quintero <anitaquintero99@gmail.com>
2022-01-24 23:09:44 -0600

    new exercise, time

--- python/time.py
+++ python/time.py
@@ -1,10 +1,13 @@
 def time(seconds):
     minutes = 60
     hour = 3600
-    if seconds >= minutes:
+    if seconds <= minutes:
         mm = seconds / minutes
-        print(f"{mm}:{minutes-1}")
+        print(f"00:{int(mm)}:{minutes-1}")
+    elif seconds >= hour:
+        hh = seconds / hour
+        print(f"{int(hh)}:{minutes-1}:{minutes-1}")
 
 
-seconds = 60
+seconds = 359999
 time(seconds)
